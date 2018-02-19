3b44eced9c7db59f14c01d520aee673f7a1c70a3
sidharthbhatia-orangewood <sidharth.b@orangewood.co>
2018-02-19 13:45:55 +0000

    19/02/2018

---
+++ OpenCV/draw_filled_contour+.py
@@ -0,0 +1,79 @@
+#importing required packages
+import numpy as np
+import cv2
+
+#reading source image from file
+img = cv2.imread('/home/pi/Downloads/3.jpg')
+cv2.imshow('Image',img)
+cv2.waitKey(0)
+
+#converting source image to grayscale
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#applying a smoothing filter to the grayscale image
+blur = cv2.GaussianBlur(gray_image,(3,3),0)
+cv2.imshow('Blurred-Image',blur)
+cv2.waitKey(0)
+
+#Thresholding the blurred image
+ret,thresh1 = cv2.threshold(blur,220,255,cv2.THRESH_BINARY)
+cv2.imshow('Thresholded-Image', thresh1)
+cv2.waitKey(0)
+
+#drawing contours on processed image
+contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+#finding image dimensions to compute area
+height, width, channels = img.shape
+print height, width, channels
+
+#finding number of contours
+print('len(contours)=',len(contours))
+
+#locating child contours
+print(hierarchy)
+a = hierarchy[0,0,2]
+print ('a=', a)
+b = hierarchy[0,a,0]
+print  ('b=', b)
+
+#this check is performed to eliminate smaller shapes
+#that may be contoured due to noise/ lighting
+#also includes the point polygon test
+if (cv2.contourArea(contours[a])>cv2.contourArea(contours[b])):
+ cimg=np.zeros_like(img)
+ cv2.drawContours(cimg, contours, a, (0,255,0), cv2.cv.CV_FILLED)
+ #cv2.fillPoly(img, pts = contours[a], color = (255,255,255))
+ cv2.imshow('Sid-Image', cimg)
+ cv2.waitKey(0)
+ x,y,w,h = cv2.boundingRect(contours[a])
+ #cv2.rectangle(cimg,(x-10,y-10),(x+w+10,y+h+10),(0,0,255),2)
+ x=x-10
+ y=y-10
+ timg=np.zeros_like(img)
+ for y in range (y,y+h+20):
+  
+  print(x,x+w+20)
+  for i in range (x,x+w+20):
+   print('x:', x,'y:',y)
+   a = cimg[y,i]
+   if(a[1] == 255):
+    timg[y,i] = [0,0,255]
+    
+ cv2.imshow('Sid-Image-copy', timg)
+ cv2.waitKey(0)
+ 
+
+ #dist = cv2.pointPolygonTest(contours[a],(34,254),True)
+ #print('dist=', dist)
+else:
+ cv2.drawContours(img, contours, b, (0,255,0), 5)
+ dist = cv2.pointPolygonTest(contours[b],(50,50),True)
+ print('dist=', dist)
+
+#display contour of shape
+cv2.imshow('CONTOURS',img)
+cv2.waitKey(0)
+
+
+cv2.destroyAllWindows
