ddb3a044a48424a1fbcb2842bef934593053a906
sidharthbhatia-orangewood <sidharth.b@orangewood.co>
2018-02-20 11:50:12 +0000

    20/02/2018 17:20

--- HARDWARE/codes/Ultrasound_sensor.py
+++ HARDWARE/codes/Ultrasound_sensor.py
@@ -15,23 +15,25 @@ GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
 time.sleep(2)
 
-#Trigger pulse
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+while True:
+ #Trigger pulse
+ GPIO.output(TRIG, True)
+ time.sleep(0.00001)
+ GPIO.output(TRIG, False)
 
-#Wait for pulse to return
-while GPIO.input(ECHO)==0:
- pulse_start = time.time()
-while GPIO.input(ECHO)==1:
- pulse_end = time.time()
+ #Wait for pulse to return
+ while GPIO.input(ECHO)==0:
+  pulse_start = time.time()
+ while GPIO.input(ECHO)==1:
+  pulse_end = time.time()
 
-#Calculating time
-pulse_time = pulse_start - pulse_end
+ #Calculating time
+ pulse_time = pulse_end-pulse_start
+ 
+ #Calculating distance approximating speed of sound to be 34300
+ dist = 17150*pulse_time
 
-#Calculating distance approximating speed of sound to be 34300
-dist = 17150*pulse_time*100
-
-print ('Distance: ', dist, ' mm')
+ print ('Distance:',dist,' cm')
+ time.sleep(1) 
 
 GPIO.cleanup()
